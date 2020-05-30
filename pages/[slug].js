c21ab0f19660261ead09096a1f16b4108e1cf85a
abidurr <42abid@gmail.com>
2020-05-30 00:16:52 -0400

    Showing raw md files

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,33 +1,42 @@
 import React from "react";
 import fs from "fs";
+import path from "path";
 
-const Post = ({slug}) => {
-  return (<div> Slug: {slug} </div>)
+const Post = ({ contents }) => {
+    return (
+        <div>
+            <div>Contents Below</div>
+            <pre>{contents}</pre>
+        </div>
+    );
 };
 
 export const getStaticPaths = async () => {
-  const files = fs.readdirSync("posts");
-  console.log("files: ", files);
+    const files = fs.readdirSync("posts");
+    console.log("files: ", files);
 
-  const paths = files.map( filename => ({
-    params: {
-      slug: filename.replace(".md", "")
-    }
-  }));
-  console.log("paths: ", paths);
+    const paths = files.map((filename) => ({
+        params: {
+            slug: filename.replace(".md", ""),
+        },
+    }));
+    console.log("paths: ", paths);
 
-  return {
-    paths,
-    fallback: false
-  };
+    return {
+        paths,
+        fallback: false,
+    };
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
-  return {
-    props: {
-      slug
-    }
-  }
-}
+  // convert buffer to string and join for any OS
+    const contents = fs.readFileSync(path.join("posts", slug + ".md")).toString();
+
+    return {
+        props: {
+            contents,
+        },
+    };
+};
 
 export default Post;
